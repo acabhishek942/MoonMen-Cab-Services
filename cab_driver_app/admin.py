ec9e3469014ef1e6003010ce0b88450673d46f53
Abhishek Choudhary <acabhishek942@gmail.com>
2018-10-31 01:54:19 +0530

    registered models with admin and made migrations

--- cab_driver_app/admin.py
+++ cab_driver_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import CabDriver, Cab, Shift
+
 # Register your models here.
+
+admin.site.register(Cab)
+admin.site.register(CabDriver)
+admin.site.register(Shift)
+
--- cab_user_app/admin.py
+++ cab_user_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import CabUser, CabRide, CabRideStatus
+
 # Register your models here.
+admin.site.register(CabRide)
+admin.site.register(CabUser)
+admin.site.register(CabRideStatus)
